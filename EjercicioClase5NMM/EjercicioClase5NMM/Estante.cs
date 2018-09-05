0a57c751caec9b7d362a7e73c5df6a2228ba05ff
nmiguenz <37988654+nmiguenz@users.noreply.github.com>
2018-09-04 22:21:06 -0300

    Integrador - Clase 5 (falta terminar)

---
+++ EjercicioClase5NMM/EjercicioClase5NMM/Estante.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase5NMM
+{
+    class Estante
+    {
+        private int ubicacionEstante;
+        private Producto[] productos;
+
+        private Estante(int capacidad)
+        {
+            productos = new Producto[capacidad];
+        }
+
+        // Los dos puntos lo que haces es heredar del private Estante() de arriba
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.productos = new Producto[ubicacion];
+        }
+
+        public Producto[] GetProducto()
+        {
+            return productos;
+        }
+
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder mystringBuielder = new StringBuilder();
+
+            foreach (Producto producto in e.productos)
+            {
+                mystringBuielder.AppendFormat("Producto : {0}, ubicacion {1}", producto.GetMarca(), e.ubicacionEstante);
+            }
+
+            return mystringBuielder.ToString();
+        }
+
+        public static bool operator ==(Estante e, Producto p)
+        {
+            foreach (Producto productoEstante in e.productos)
+            {
+                if (productoEstante == p)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return false;
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            // Si el Estante no contiene el Producto...
+            if (e != p)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (Object.ReferenceEquals(e.productos[i], null) )
+                    {
+                        e.productos[i] = p;
+
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool operator -(Estante e, Producto p)
+        {
+            if (e !=)
+        }
+            
+    }
+}
---
+++ EjercicioClase5NMM/EjercicioClase5NMM/Producto.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase5NMM
+{
+    class Producto
+    {
+        //ATRIBUTOS
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        // MÉTODOS
+        public string GetMarca()
+        {
+            return marca;
+        }
+
+        public float GetPrecio()
+        {
+
+            return precio;
+        }
+
+        // Siempre que el metodo sea de clase usar "static" y acceder con el operador punto.
+        public static string MostrarProducto(Producto p)
+        {
+
+            return string.Format("Marca: {0}, Codigo de barras: {1}, precio: {2}", p.marca, p.codigoDeBarra, p.precio);
+        }
+
+        public Producto(string codigo, string marca, float precio)
+        {
+            this.codigoDeBarra = codigo;
+            this.marca = marca;
+            this.precio = precio;
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public static bool operator == (Producto p1, Producto p2)
+        {
+            return (p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra);
+        }
+
+        public static bool operator != (Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+        /*
+         * Igualdad (Producto, string). Retornará true, si la marca del producto coincide con la cadena pasada por parámetro, false,
+caso contrario
+         */
+         public static bool operator == (Producto p1, string marca)
+        {
+            return p1.marca == marca;
+        }
+
+        public static bool operator !=(Producto p1, string marca)
+        {
+            return !(p1 == marca);
+        }
+    }
+}
