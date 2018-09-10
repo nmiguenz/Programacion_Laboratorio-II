4313d4703c6a91e26840e6c99470c12e35fd8059
nmiguenz <37988654+nmiguenz@users.noreply.github.com>
2018-09-10 15:30:32 -0300

    Ejercicio terminado. Integrador

--- EjercicioClase5NMM/EjercicioClase5NMM/Estante.cs
+++ EjercicioClase5NMM/EjercicioClase5NMM/Estante.cs
@@ -11,22 +11,40 @@ namespace EjercicioClase5NMM
         private int ubicacionEstante;
         private Producto[] productos;
 
+        /// <summary>
+        /// Inicializo un nuevo estante, segun la capacidad.
+        /// </summary>
+        /// <param name="capacidad"></param>
         private Estante(int capacidad)
         {
             productos = new Producto[capacidad];
         }
 
         // Los dos puntos lo que haces es heredar del private Estante() de arriba
+        /// <summary>
+        /// Inicializo un nuevo estante, asignando los atributos capacidad y ubicación.
+        /// </summary>
+        /// <param name="capacidad">Cantidad de productos que podrá contener el estante</param>
+        /// <param name="ubicacion">Código de ubicación del estante</param>
         public Estante(int capacidad, int ubicacion) : this(capacidad)
         {
             this.productos = new Producto[ubicacion];
         }
 
+        /// <summary>
+        /// Obtengo la lista de productos
+        /// </summary>
+        /// <returns></returns>
         public Producto[] GetProducto()
         {
-            return productos;
+            return this.productos;
         }
 
+        /// <summary>
+        /// Se muestran los datos del estante elegido
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
         public static string MostrarEstante(Estante e)
         {
             StringBuilder mystringBuielder = new StringBuilder();
@@ -39,34 +57,23 @@ namespace EjercicioClase5NMM
             return mystringBuielder.ToString();
         }
 
+        /// <summary>
+        /// Analiza si el producto esta en el estante
+        /// </summary>
+        /// <param name="e"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
         public static bool operator ==(Estante e, Producto p)
         {
+            // Recorro la lista de productos del estante
             foreach (Producto productoEstante in e.productos)
             {
-                if (productoEstante == p)
+                // Valido que la posición del arrey no sea null
+                if (!object.ReferenceEquals(productoEstante, null))
                 {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public static bool operator !=(Estante e, Producto p)
-        {
-            return false;
-        }
-
-        public static bool operator +(Estante e, Producto p)
-        {
-            // Si el Estante no contiene el Producto...
-            if (e != p)
-            {
-                for (int i = 0; i < e.productos.Length; i++)
-                {
-                    if (Object.ReferenceEquals(e.productos[i], null) )
+                    // Utilizo la sobrecarga del == de Producto
+                    if (productoEstante == p)
                     {
-                        e.productos[i] = p;
-
                         return true;
                     }
                 }
@@ -74,9 +81,62 @@ namespace EjercicioClase5NMM
             return false;
         }
 
-        public static bool operator -(Estante e, Producto p)
+        /// <summary>
+        /// Analiza si el producto NO esta en el Estante
+        /// </summary>
+        /// <param name="e"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static bool operator !=(Estante e, Producto p)
         {
-            if (e !=)
+            return !(e == p);
+        }
+
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            // Si el Estante no contiene el Producto...
+            if (e != p)
+            {
+                // Recorro la lista de productos del estante
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    // Busco un espacio vacio
+                    if (Object.ReferenceEquals(e.productos[i], null) )
+                    {
+                        e.productos[i] = p;
+                        // Si se agrega el producto, rompo el lazo iterativo
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Saca un producto del estante
+        /// </summary>
+        /// <param name="e"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static Estante operator -(Estante e, Producto p)
+        {
+            // Recorro la lista de productos del estante...
+            for (int i = 0; i < e.productos.Length; i++)
+            {
+                // Valido que la posición del Array no sea null
+                if (!object.ReferenceEquals(e.productos[i], null))
+                {
+                    if (p == e.productos[i])
+                    {
+                        // Vacio el espacio que ocupaba dicho Producto
+                        e.productos[i] = null;
+                        // Rompo el lazo
+                        break;
+                    }
+                }
+            }
+            return e;
         }
             
     }
