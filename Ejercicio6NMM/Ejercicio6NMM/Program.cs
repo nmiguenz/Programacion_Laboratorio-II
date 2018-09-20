5968386c75918d16bd4e8081d3beec4eb5e4cf47
nmiguenz <37988654+nmiguenz@users.noreply.github.com>
2018-09-20 17:15:31 -0300

    Año bisiesto (Terminado)

---
+++ Ejercicio6NMM/Ejercicio6NMM/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6NMM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int añoInicio;
+            int añoFin;
+            Console.WriteLine("Ingrese año de inicio");
+            int.TryParse(Console.ReadLine(), out añoInicio);
+            Console.WriteLine("Ingrese año final");
+            int.TryParse(Console.ReadLine(), out añoFin);
+
+            for (int i=añoInicio; i<=añoFin; i++)
+            {
+                if(i % 4 == 0 && (i % 100 != 0 || i % 400 == 0))
+                {
+                    Console.WriteLine("{0}", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
