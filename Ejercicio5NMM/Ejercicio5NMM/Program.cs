6f2c8b05a57de3607da222dd88e2a88c5686f665
nmiguenz <37988654+nmiguenz@users.noreply.github.com>
2018-09-20 17:14:29 -0300

    Centro numérico (no terminado)

---
+++ Ejercicio5NMM/Ejercicio5NMM/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5NMM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int numeroIncremental = 0;
+            int acumMenor;
+            int acumMayor;
+            Console.WriteLine("Ingrese un numero para buscar cuales son los centros numéricos");
+            int.TryParse(Console.ReadLine(), out numero);
+
+            for(int i=0; i<numero; i++)
+            {
+                acumMenor = 0;
+                for (int j=i-1; j>0;j--)
+                {
+                    acumMenor += j;
+                }
+
+                numeroIncremental = i++;
+                acumMayor = 0;
+                do
+                {
+                    acumMayor += numeroIncremental;
+                } while (acumMayor <= acumMenor);
+
+                if (Object.ReferenceEquals(acumMenor, acumMayor))
+                {
+                    Console.WriteLine("El centro numérico es: {0}", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
