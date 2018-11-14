a1195ec55254101f1d62320d2bcb556f3806661d
nmiguenz <37988654+nmiguenz@users.noreply.github.com>
2018-11-13 22:02:47 -0300

    Segundo Parcial

---
+++ 20180628-SP-Nicolas.Miguenz/20180628-SP - Alumno/Entidades/Dao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Entidades
+{
+    public class Dao<Votacion> : IArchivos<Votacion>
+    {
+        public bool Guardar(string rutaArchivo, Votacion objeto)
+        {
+            string connectionStr = "Data Source = ./SQLEXPRESS; Initial Catalog= votacion-sp-2018; Integrated Security=True";
+            SqlConnection conexion;
+
+            conexion = new SqlConnection(connectionStr);
+            string consulta;
+
+            try
+            {
+                consulta = String.Format("INSERT INTO Votaciones (nombreLey, afirmativos, negativos, abstenciones, nombreAlumno) VALUES ('{0}',{1},{2},{3},'{4}')", objeto.)
+            }
+            
+
+        }
+
+        public Votacion Leer(string rutaArchivo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 20180628-SP-Nicolas.Miguenz/20180628-SP - Alumno/Entidades/SerializarXML.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using Excepcion;
+
+namespace Entidades
+{
+    class SerializarXML<T>:IArchivos<T>
+    {
+        public T Leer(string rutaArchivo)
+        {
+            T leido;
+            XmlTextReader reader;
+            XmlSerializer serializa;
+
+            reader = new XmlTextReader(rutaArchivo);
+
+            serializa = new XmlSerializer(typeof(T));
+
+            leido = (T)serializa.Deserialize(reader);
+
+            reader.Close();
+            return leido;
+        }
+        public bool Guardar(string rutaArchivo, T objeto)
+        {
+            bool retorno = false;
+            XmlTextWriter writer=null;
+            XmlSerializer serializador;
+
+            try
+            {
+                writer = new XmlTextWriter(rutaArchivo, Encoding.UTF8);
+                serializador = new XmlSerializer(typeof(T));
+                serializador.Serialize(writer, objeto);
+                retorno = true;
+            }
+            catch(Exception ex)
+            {
+                throw new ErrorArchivoException();
+            }
+            finally
+            {
+                writer.Close();
+            }
+            return retorno;            
+        }
+
+        
+    }
+}
