b9e20c9bdc8845d2b559a23571c0743264ac0d29
Nicolás Miguenz <nmiguenz@gmail.com>
2020-07-26 19:30:45 -0300

    Falta Test Unitario. No funciona serialización

---
+++ Final/Prácticas/20180628-Votacion/20180628-SP - Alumno/Entidades/Dao.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace Entidades
+{
+    public class Dao : IArchivos<Votacion>
+    {
+        public bool Guardar(string rutaArchivo, Votacion objeto)
+        {
+            bool todoOk = false;
+            string nombreAlumno = "Nicolas Miguenz";
+            String connectionStr = "Data Source=LAPTOP-NICO\\SQLEXPRESS;Initial Catalog=votacion-sp-2018;Integrated Security=True";
+            string consulta = String.Format("INSERT INTO Votaciones (nombreLey, afirmativos, negativos, abstenciones, nombreAlumno)" + "VALUES ('{0}','{1}','{2}','{3}','{4}')", 
+                              objeto.NombreLey, objeto.ContadorAfirmativo, objeto.ContadorNegativo, objeto.ContadorAbstencion, nombreAlumno);
+
+            SqlConnection conexion = null;
+            SqlCommand comando;
+
+            try
+            {
+                //instacia de la conexion
+                conexion = new SqlConnection(connectionStr);
+                //instancia del comando
+                comando = new SqlCommand();
+                comando.CommandType = System.Data.CommandType.Text;
+                //Paso la instruccion SQL a procesar
+                comando.CommandText = consulta;
+                //
+                comando.Connection = conexion;
+                conexion.Open();
+                //Ejecuta la consulta SQL
+                comando.ExecuteNonQuery();
+                todoOk = true;
+            }
+            catch (Exception e)
+            {
+                //relanza la excepcion capturada
+                throw (e);
+            }
+            finally
+            {
+                if (conexion != null)
+                {
+                    conexion.Close();
+                }
+            }
+            return todoOk;
+        }
+
+        public Votacion Leer(string rutaArchivo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Final/Prácticas/20180628-Votacion/20180628-SP - Alumno/Entidades/SerializarXML.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Excepciones;
+
+namespace Entidades
+{
+    public class SerializarXML<T> : IArchivos<T>
+    {
+        public bool Guardar(string rutaArchivo, T objeto)
+        {
+
+            XmlTextWriter writer = null;
+            XmlSerializer ser;
+
+            try
+            {
+                ser = new XmlSerializer(typeof(T));
+                writer = new XmlTextWriter(rutaArchivo, Encoding.UTF8);
+                ser.Serialize(writer, objeto);
+                writer.Close();
+            }
+            catch (Exception e)
+            {
+
+                throw new ErrorArchivoException(e.Message, e);
+            }
+            finally
+            {
+                if(!(writer is null))
+                {
+                    writer.Close();
+                }
+            } 
+            return true;
+        }
+        
+        public T Leer(string rutaArchivo)
+        {
+            XmlSerializer reader;
+            StreamReader file = null;
+            T objetoLeido;
+            try
+            {
+                //Instancia del objeto XmlSerializer
+                reader = new XmlSerializer(typeof(T));
+                //Lee la ruta del archivo
+                file = new StreamReader(rutaArchivo);
+                //Deserializacion del objeto del tipo necesario
+                objetoLeido = (T)reader.Deserialize(file);
+            }
+            catch (Exception e)
+            {
+
+                throw new ErrorArchivoException(e.Message, e);
+            }
+            finally
+            {
+                if (file != null)
+                {
+                    file.Close();
+                }
+            }
+            return objetoLeido;
+        }
+    }
+}
